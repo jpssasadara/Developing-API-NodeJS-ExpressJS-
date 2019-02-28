94236dc8ed9ec871a6d60000588e39ba85aa7e1a
jps sasadara <jpssasadara1995@gmail.cpm>
2019-03-01 00:39:30 +0530

    Update

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        massage:'Handling GET request to /products'
+    });
+});
+
+router.post('/',(req,res,next)=>{
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        massage:'Handling post request to /products',
+        createdproduct: product
+    });
+});
+
+router.get('/:productId',(req,res,next)=>{
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+           massage:'You discovered the special ID',
+           id: id 
+        });
+    }else{
+        res.status(200).json({
+            massage:'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        massage: 'Update product !'
+    });
+});
+
+router.delete('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        massage: 'Delete product !'
+    });
+});
+
+module.exports=router;
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+/*app.use((req,res,next)=>{
+    res.status(200).json({
+        message: 'It works !'
+    });
+});*/
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Routes which should handle requests
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
+// for handling invalied URLs------------>>FROM
+app.use((req,res,next)=>{
+    const error = new Error('Not found OOOH');
+    error.status = 404;
+    next(error);
+})
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message:error.message
+        }
+    });
+});
+//------------------------------------------ TO
+
+module.exports = app; 
