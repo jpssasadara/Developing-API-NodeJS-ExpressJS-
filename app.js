7866f2c6512d053f867d54f5429e485bb826c22d
jps sasadara <jpssasadara1995@gmail.cpm>
2019-03-02 20:25:06 +0530

    Add and select Api

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://sasadara:'+process.env.MONGO_ATLAS_PW+'@node-rest-shop-shard-00-00-bxdsl.mongodb.net:27017,node-rest-shop-shard-00-01-bxdsl.mongodb.net:27017,node-rest-shop-shard-00-02-bxdsl.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true'
+/*{
+  useMongoClient:true
+}*/
+);
 
 /*app.use((req,res,next)=>{
     res.status(200).json({
@@ -15,6 +22,20 @@ const orderRoutes = require('./api/routes/orders');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//Handling CORS error
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE')
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //Routes which should handle requests
 app.use('/products',productRoutes);
