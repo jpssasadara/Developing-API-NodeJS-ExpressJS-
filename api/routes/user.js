71fdeafc076d9cbe41f72533688a17474ae1da2c
jps sasadara <jpssasadara1995@gmail.cpm>
2019-03-09 17:08:49 +0530

    login

--- api/routes/user.js
+++ api/routes/user.js
@@ -64,4 +64,38 @@ router.delete("/:userId",(req,res,next) =>{
         });
     });
 });
+
+router.post("/login",(req, res, next)=>{
+    User.find({email: req.body.email})
+    .exec()
+    .then(user =>{
+        if(user.length < 1){
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+       //according to bcrypt github page
+       bcrypt.compare(req.body.password, user[0].password, (err,result)=>{
+           if(err){
+               return res.status(401).json({
+                   message: 'Auth failed'
+               });
+           }
+           if(result){
+               return res.status(200).json({
+                   message: 'Auth successfull'
+               });
+           }
+           res.status(401).json({
+               message: 'Auth failed'
+           });
+       }); 
+    })
+    .catch(err =>{
+        console.log(err); 
+        res.status(500).json({
+            error: err
+        });
+    });
+})
 module.exports = router;
